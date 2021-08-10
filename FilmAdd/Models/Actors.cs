7c7a856b2efb7979f93a52169aed74e7719dbcfd
Vladislav <morisonadd@mail.ru>
2021-08-10 15:01:55 +0500

    Load project

---
+++ FilmAdd/Models/Actors.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FilmAdd.Models
+{
+    public class Actors
+    {
+        public string Id { get; set; }
+        public string FirstName { get; set; }
+        public string  LastName { get; set; }
+
+        public override string ToString()
+        {
+            return FirstName;
+        }
+    }
+}
---
+++ FilmAdd/Models/Film.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FilmAdd.Models
+{
+    public class Film
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public int Rating { get; set; }
+        public string ImageURL { get; set; }
+
+        public override string ToString()
+        {
+            return Name;
+        }
+    }
+}
