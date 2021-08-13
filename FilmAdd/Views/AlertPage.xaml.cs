6ef699ca8739e664c76e294843b1942b0fd2a24e
Vladislav <morisonadd@mail.ru>
2021-08-13 12:10:56 +0500

    Adeed a library page

---
+++ FilmAdd/Views/AlertPage.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace FilmAdd.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class AlertPage : ContentPage
+    {
+        public AlertPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void OnDisplayAlertButtonClicked(object sender, EventArgs e)
+        {
+            await DisplayAlert("Alert", "This is an alert", "OK");
+        }
+        private async void OnDisplayAlertQuestionButtonClicked(object sender, EventArgs e)
+        {
+            bool responce = await DisplayAlert("Save", "Would you like to save your Data?", "Yes", "No");
+            Console.WriteLine("Save data:" + responce);
+        }
+    }
+}
---
+++ FilmAdd/Views/LibraryPage.xaml.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using FilmAdd.Models;
+
+namespace FilmAdd.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class LibraryPage : ContentPage
+    {
+        public IList<Film> Films { get; private set; }
+        public LibraryPage()
+        {
+            InitializeComponent();
+
+            Films = new List<Film>();
+
+            Films.Add(new Film
+            {
+                Id = "1",
+                Name = "Astral",
+                Description = "This cool film",
+                Rating = 10,
+                ImageURL = "https://www.film.ru/sites/default/files/filefield_paths/16361569-1006838.jpg"
+            });
+            Films.Add(new Film
+            {
+                Id = "2",
+                Name = "Форсаж",
+                Description = "This cool film",
+                Rating = 8,
+                ImageURL = "https://images.kinorium.com/movie/shot/1561985/w1500_47188924.jpg"
+            });
+            Films.Add(new Film
+            {
+                Id = "3",
+                Name = "Оно",
+                Description = "This cool film",
+                Rating = 9,
+                ImageURL = "https://avatars.mds.yandex.net/get-zen_doc/61795/pub_5d7792d47cccba00ad0f2668_5d77942ee6cb9b00ae2c53a2/scale_1200"
+            });
+            Films.Add(new Film
+            {
+                Id = "4",
+                Name = "Зомби ленд",
+                Description = "This cool film",
+                Rating = 10,
+                ImageURL = "https://avatars.mds.yandex.net/get-zen_doc/1866022/pub_5db2f56198930900ad0567a3_5db2f71b8600e100b084faa5/scale_1200"
+            });
+            BindingContext = this;
+        }
+        private void OnSelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            Film selectedItem = e.CurrentSelection[0] as Film;
+
+        }
+
+        private async void BackToRoot(object sender, EventArgs e)
+        {
+            await Navigation.PopToRootAsync();
+        }
+    }
+}
